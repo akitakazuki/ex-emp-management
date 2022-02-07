830d9803b6278365538a8232b91ee4582fdd4e92
akitakazuki <kazuki.akita@rakus-partners.co.jp>
2022-02-07 12:43:57 +0900

    ログイン画面表示

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,28 +1,68 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
+	
 	@Autowired
 	private AdministratorService administratorService;
+	private HttpSession session;
+	
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
+	
+	//ログイン画面
+	@RequestMapping("/login")
+	public String login(LoginForm form,Model model) {
+		Administrator result= administratorService.login(form.getMailAddress(), form.getPassword());
+		if(result==null) {
+			model.addAttribute("form", "メールアドレスまたはパスワードが不正です。");
+			return "/login";
+		}
+		session.setAttribute("administratorName", "管理者");
+		 return "forward:/employee/list";
+	}
+	
+	//ログイン画面リダイレクト
+	@RequestMapping("/")
+	public String toLogin() {
+		return "/administrator/login";
+	}
+	
+	
 	
 	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
-		return new InsertAdministratorForm();
-		
+		return new InsertAdministratorForm();	
 	}
+	//登録画面
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		administratorService.insert(administrator);
+		return "toInsert";	
+	}
+	//登録画面リダイレクト
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "/administrator/insert";
 		
 	}
-	
 }
