5ebb7946d80b345045c8d9ad5a90944ab7d62855
akitakazuki <kazuki.akita@rakus-partners.co.jp>
2022-02-07 09:12:55 +0900

    初回commit

---
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -0,0 +1,49 @@
+package jp.co.sample.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Administrator;
+
+@Repository
+public class AdministratorRepository {
+
+	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER=(rs,i)->{
+		Administrator administrator =new Administrator();
+		administrator.setId(rs.getInt("id"));
+		administrator.setName(rs.getString("name"));
+		administrator.setName(rs.getString("mail_address"));
+		administrator.setPassword(rs.getString("password"));
+		return administrator;
+	};
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	
+	public void insert (Administrator administrator) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+		String insertSql= "insert into administrators (name,mail_address,password)"
+				+ "values(:name,:mail_address,:password)";
+		template.update(insertSql,param);
+	}
+	
+	public Administrator findByMailAddressAndPassword(String mailAddress,String password) {
+		
+		String sql = "select id,name,mail_address,password from administrators "
+				+ "where mail_address= :mailAddress and password = :password";
+		
+		List<Administrator> administratorList= template.query(sql, ADMINISTRATOR_ROW_MAPPER);
+		
+		if (administratorList.size() == 0) {
+		return null; 
+		}
+		return administratorList.get(0);
+	}
+}
